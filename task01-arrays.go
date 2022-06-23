507995e5f9d2467050c921a0df516089c418411f
MilDeRRy <bane009@yandex.ru>
2022-06-23 23:00:27 +0400

    0005

--- task01-arrays.go
+++ task01-arrays.go
@@ -5,7 +5,7 @@ func average(input [15]float32) (result float32) {
 	for _, v := range input {
 		res += v
 	}
-	res = res / 6
+	res = res / 15
 	return res
 }
 
@@ -17,13 +17,17 @@ func main() {
 	results := [15]float32{1, 2, 3, 4, 5, 6}
 	grim := average(results)
 	fmt.Println(grim)
+	//fmt.Println(len(results))
+	//fmt.Println(results)
 }
 
 func average(input [15]float32) (result float32) {
 	var res float32 = 0
+	//var uid float32
 	for _, v := range input {
 		res += v
 	}
-	res = res / 6
+	//(len(input))
+	res = res / 15
 	return res
 }*/
